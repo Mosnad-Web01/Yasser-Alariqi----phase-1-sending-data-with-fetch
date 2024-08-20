4a3d784338590792cc251dfaeb8b5d609b62e5c9
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-20 23:57:50 +0300

    Add the code and run a successful test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+let submitData = (name, email) => {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      document.body.innerHTML += `<div>${data.name} ${data.email} ${data.id}</div>`;
+    })
+    .catch((error) => {
+      document.body.innerHTML += `<div>${error.message}</div>`;
+      console.error(error);
+    });
+};
